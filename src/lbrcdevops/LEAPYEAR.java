e0f8e82ae4d206ba4a40e175044402f4918f7990
AdmiN <AdmiN@ITWS069>
2024-08-20 11:18:56 +0530

    commit

--- src/lbrcdevops/LEAPYEAR.java
+++ src/lbrcdevops/LEAPYEAR.java
@@ -4,6 +4,21 @@ public class LEAPYEAR {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		  boolean is_leap_year = false;
+		  
+	        // If year is divisible by 4
+	        if (year % 4 == 0) {
+	            is_leap_year = true;
+	 
+	            // To identify whether it is a
+	            // century year or not
+	            if (year % 100 == 0) {
+	                // Checking if year is divisible by 400
+	                // therefore century leap year
+	                if (year % 400 == 0)
+	                    is_leap_year = true;
+	                else
+	                    is_leap_year = false;
 
 	}
 
