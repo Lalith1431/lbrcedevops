ddd28f5a89c097f1463b863e59af38d2982701f7
AdmiN <AdmiN@ITWS069>
2024-08-20 11:02:44 +0530

    commit

--- src/lbrcdevops/loginform.java
+++ src/lbrcdevops/loginform.java
@@ -4,7 +4,26 @@ public class loginform {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	        Scanner scanner = new Scanner(System.in);
 
-	}
+	        System.out.print("Please enter your name: ");
 
+	        
+
+	        // Read the user's input
+
+	        String userName = scanner.nextLine();
+
+	        
+
+	        // Display a personalized welcome message
+
+	        System.out.println("Welcome to the app, " + userName + "!");
+
+
+
+	        // Close the Scanner object to prevent resource leaks
+
+	        scanner.close();
 }
